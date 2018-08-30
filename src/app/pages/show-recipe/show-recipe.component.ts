9179af26a0dee13568299b99d74b1c55957160f3
Sumaya <smymohd@gmail.com>
2018-08-29 23:23:38 -0500

    final

---
+++ src/app/pages/show-recipe/show-recipe.component.ts
@@ -0,0 +1,42 @@
+import { Recipe } from './../../classes/recipe'; 
+import { RecipeService } from './../../services/recipe.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { Subscription } from 'rxjs/Subscription';
+
+@Component({
+  selector: 'app-show-recipe',
+  templateUrl: './show-recipe.component.html',
+  styleUrls: ['./show-recipe.component.scss']
+})
+export class ShowRecipeComponent implements OnInit, OnDestroy {
+  private subscription: Subscription[] = [];
+  public recipe: Recipe;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private recipeService: RecipeService,
+    private location: Location
+  ) { }
+
+  ngOnInit() {
+    this.subscription.push(
+      this.route.paramMap.subscribe(params =>{
+        const recipeId = params.get('id');
+        this.recipe = this.recipeService.getRecipeById(parseInt(recipeId));
+      })
+    )
+  }
+  ngOnDestroy() {
+    this.subscription.forEach(sub => sub.unsubscribe());
+  }
+  back() {
+    this.location.back();
+  }
+  deleteRecipe(): void {
+    this.recipeService.deleteRecipe(this.recipe.id);
+    this.router.navigate(['']);
+  }
+}
