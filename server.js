c67e945cea779693f1b9d596efb233ee42d2845e
Sumaya <smymohd@gmail.com>
2018-08-30 21:13:28 -0500

    heroku

---
+++ server.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/dist'));
+
+app.listen(process.env.PORT || 8080);
+
+//PATHLOCATIONSTRATEGY
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+})
+console.log('Console listening!');
