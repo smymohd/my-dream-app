7f16a47aac3b30253537f426ebb6f92687892273
Sumaya <smymohd@gmail.com>
2018-08-31 20:10:52 -0500

    new

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.listen(process.env.PORT || 8080);
 //PATHLOCATIONSTRATEGY
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/my-dream-app/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 })
 console.log('Console listening!');
